c1a55c3b4d58d8b99760a1706acdc16ba488fe8e
mik284 <mikeykamau222@gmail.com>
2022-11-26 22:48:30 +0300

    Created add and reduce quantity fro cart products

--- E-commerce/Backend/controllers/cartControllers.js
+++ E-commerce/Backend/controllers/cartControllers.js
@@ -41,16 +41,26 @@ module.exports={
     },
     incQuantity: async (req, res) => {
         try {
+             const {id} = req.params
+            const pool = await mssql.connect(sqlConfig)
+            await pool.request().input('id', id).execute('incQuantity')
+
+            res.status(200).json("Added quantity to the product")
             
         } catch (error) {
-            
+            res.status(500).json({message: error.message})
         }
     },
     decQuantity: async (req, res) => {
         try {
+              const {id} = req.params
+            const pool = await mssql.connect(sqlConfig)
+            await pool.request().input('id', id).execute('decQuantity')
+
+            res.status(200).json("reduced quantity to the product")
             
         } catch (error) {
-            
+            res.status(500).json({message: error.message})
         }
     },
     clearCart: async (req, res) => {
