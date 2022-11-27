bc5c58cdf12ef11e0d1622988933c958296ba72a
mik284 <mikeykamau222@gmail.com>
2022-11-27 18:44:46 +0300

    Added node cron on bg service

--- E-commerce/BackgroundServices/index.js
+++ E-commerce/BackgroundServices/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const sqlConfig = require('./config/config')
+const cronJob = require('node-cron')
 
 dotenv.config()
 
 
 const app = express();
-
 app.use(express.json());
+
+const start = () =>{
+ cronJob.schedule('2 * * * * ', () => {
+  console.log('running a task every 2 minute');
+})}
+start()
 
 
 app.listen(process.env.PORT, ()=>{
